cbe461e46d353165814f0bf678b45f03df951bed
Valeria <Valeria@LAPTOP-475BS1BM>
2019-03-11 13:52:12 +0100

    Aggiunti metodi Standard

--- src/it/polito/tdp/Laboratorio/Voto.java
+++ src/it/polito/tdp/Laboratorio/Voto.java
@@ -6,6 +6,37 @@ import java.time.*;
 public class Voto {
 	private int voto;
 	private String corso;
+	//Per usare una data usiamo localdata, la più recente, è la migliore.
 	private LocalDate data;
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	//Portiamo il progetto sotto un sistema di controllo e revisione così:
+	// progetto - tastoDestro- Team - Share Project
+	//Ripeto e Faccio commit
+	
+	
 	
 }
