1c3f79bc1877ab4601c4c2be8eb7e3f9417bb1cc
Valeria <Valeria@LAPTOP-475BS1BM>
2019-03-11 13:49:52 +0100

    Progetto iniziale Classe vuota

---
+++ src/it/polito/tdp/Laboratorio/Voto.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.Laboratorio;
+
+import java.time.LocalDate;
+import java.time.*;
+
+public class Voto {
+	private int voto;
+	private String corso;
+	private LocalDate data;
+	
+}
